9bfbba2e64f98c391dad59c9bf9045325b6d01ea
jacks <jacks@DESKTOP-UMPEP4M.3521.dhcp.asu.edu>
2019-10-09 15:03:19 -0700

    "commmit twoooo"

--- Project2/src/cse360assign2/AddingMachine.java
+++ Project2/src/cse360assign2/AddingMachine.java
@@ -3,41 +3,71 @@ package cse360assign2;
  * @author Jackson Cromer
  * Class ID:85141
  * Assignment 2
- * https://github.com/jacksoncromer/j1
+ * https://github.com/jacksoncromer/j2
  * AddingMachine.java
  * Adding machine will be used to learn version control.
  *
  */
-public class AddingMachine {
 
+public class AddingMachine {
+/**
+ * String his will add the operators and the numbers onto the string keeping a history 
+ * of operations used.
+ */
+	
 	private int total;
+	private String his;
+	//history string.
+	
 	/**
 	 * total is a variable representing the total
 	 */
 	public AddingMachine () {
-		total = 0;  // not needed - included for clarity
+		//total;  // not needed - included for clarity
+		his = "0"; //constructors 
+		
 	}
 	/**
 	 * 
 	 * @return total is updated
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
-	
+	/**
+	 * Updates the String his with the + operator and then the value parameter
+	 * @param value is the number being added to the total and put onto the string
+	 */
 	public void add (int value) {
+		total = total + value;
+		String val = Integer.toString(value);
+		his = his + " + "+val;
 		
-	}
-	
+	} 
+	/**
+	 * Updates the String his with the - operator and then the value parameter
+	 * @param value is the number being subtracted from the total and put onto the string
+	 */
 	public void subtract (int value) {
+		total = total - value;
+		String val = Integer.toString(value);
+		his = his + " - "+val; 
 		
 	}
-		
-	public String toString () {
-		return "";
-	}
-
-	public void clear() {
 	
+	/**
+	 * returns the history string, operators with added and subtracted integers 
+	 * put onto the string
+	 */
+	public String toString () {
+		
+		return his;
+		
+	}
+	/**
+	 * clear will reset total to 0;
+	 */
+	public void clear() {
+		total = 0;
 	}
 }
---
+++ Project2/src/cse360assign2/AddingMain.java
@@ -0,0 +1,15 @@
+package cse360assign2;
+
+public class AddingMain {
+
+	public static void main(String[] args) {
+		AddingMachine t1 = new AddingMachine();
+		t1.add(4);
+		t1.subtract(2);
+		t1.add(5);
+		
+		System.out.println(t1.toString());
+	
+	}
+
+}
