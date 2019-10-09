db48192231ab7b864f52082da24a148451fe99c8
jacks <jacksoncromer@gmail.com>
2019-10-09 12:57:02 -0700

    "commit project"

---
+++ Project2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,43 @@
+package cse360assign2;
+/**
+ * @author Jackson Cromer
+ * Class ID:85141
+ * Assignment 2
+ * https://github.com/jacksoncromer/j1
+ * AddingMachine.java
+ * Adding machine will be used to learn version control.
+ *
+ */
+public class AddingMachine {
+
+	private int total;
+	/**
+	 * total is a variable representing the total
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	/**
+	 * 
+	 * @return total is updated
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+		
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
